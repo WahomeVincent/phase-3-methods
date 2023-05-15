8865e2da55f33a131d888186574a51999a15c06e
Wahome <vinwahome@gmail.com>
2023-05-15 15:14:15 +0300

    Completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,40 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+
+end
+
+greet_programmer
+
+def greet (name )
+    puts "Hello, #{name}!"
+end
+
+greet ("Naureen")
+
+def greet_with_default (word)
+    puts "Hello, #{word}!"
+end
+
+greet_with_default ("Naureen")
+
+def greet_with_default
+    puts "Hello, programmer!"
+end
+
+greet_with_default
+
+def add (num1, num2)
+    num1 + num2
+end
+
+add(2,3)
+
+def halve number 
+    if (number.class != "Integer") 
+        return nil
+    end
+    return number / 2
+end
+
+halve
